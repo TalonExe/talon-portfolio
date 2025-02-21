07c5420c1af1c01b8188f1545ff771b95712b061
TalonExe <liew321@hotmail.com>
2025-02-21 21:35:15 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<p>This is where the main page will be hosted</p>
+
+<!-- TODO -->
+<!-- make navbar -->
+<!-- MAKE HERO section -->
+<!-- make footer -->
+<!-- make project section -->
+<!-- make about section -->
